4d5994eaa7b7f8d92784b5f19202b7c73af95d66
dingtalk_jfkcts <a707854516@outlook.com>
2022-05-20 11:25:28 +0800

    init v4

--- src/test/java/com/tencent/wxcloudrun/FastAutoGeneratorTest.java
+++ src/test/java/com/tencent/wxcloudrun/FastAutoGeneratorTest.java
@@ -2,8 +2,10 @@ package com.tencent.wxcloudrun;
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
 
 import java.sql.SQLException;
+import java.util.Collections;
 
 public class FastAutoGeneratorTest {
 
@@ -20,16 +22,15 @@ public class FastAutoGeneratorTest {
 //        before();
         FastAutoGenerator.create(DATA_SOURCE_CONFIG)
                 // 全局配置
-                .globalConfig((scanner, builder) -> builder.author(scanner.apply("请输入作者名称")))
+                .globalConfig((scanner, builder) -> builder.author(scanner.apply("请输入作者名称"))
+                        .enableSwagger()
+                        .outputDir("/Users/apple/IdeaProjects/martialArtsSchool/src/main/java"))
                 // 包配置
-                .packageConfig((scanner, builder) -> builder.parent(scanner.apply("请输入包名")))
+                .packageConfig((scanner, builder) -> builder.parent("com.tencent.wxcloudrun")
+                        .pathInfo(Collections.singletonMap(
+                                OutputFile.xml, "/Users/apple/IdeaProjects/martialArtsSchool/src/main/resources/mapper")))
                 // 策略配置
                 .strategyConfig((scanner, builder) -> builder.addInclude(scanner.apply("请输入表名，多个表名用,隔开")))
-                /*
-                    模板引擎配置，默认 Velocity 可选模板引擎 Beetl 或 Freemarker
-                   .templateEngine(new BeetlTemplateEngine())
-                   .templateEngine(new FreemarkerTemplateEngine())
-                 */
                 .execute();
     }
 }
